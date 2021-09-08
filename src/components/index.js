c77c0dc73e4ce4cac67d75d0a3583feb18288ff4
Tyranno <eotjdgkdl@gmail.com>
2021-09-08 18:08:14 +0900

    src/components/index.js커밋

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Gallery } from './gallery';
 export { default as Programing } from './programing';
 export { default as Setting } from './setting';
 export { default as Edit } from './edit';
+export { default as View } from './view';
